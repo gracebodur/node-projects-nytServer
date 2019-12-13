0e13fbb7416eae2907c63b05ceaa4c5e1a9434c6
Grace <mgracebodur@gmail.com>
2019-12-13 01:41:47 -0500

    Built the server

--- app.js
+++ app.js
@@ -4,11 +4,34 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(morgan('common')) //common format?
-    const books = require('./books-data.js')
+    const books = require('./books-data.js') //require the array of books
 
 app.get('/books', (req, res) => {
+    const { search = " ", sort} = req.query
+
+    if (sort) {
+        if(!['title', 'rank'].includes(sort)) {
+            return res
+                .status(400)
+                .send('Sort must be one of the title or rank')
+        }
+    }
+
+    let results = books
+        .filter(book =>
+            book
+                .title
+                .toLowerCase()
+                .includes(search.toLowerCase()))
+
+    if(sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a [sort] < b[sort] ? -1 : 0
+        })
+    }
     res
-        .json(books)
+        // .json(books) //return the entire list of books first
+        .json(results)
 })
 
 app.listen(8000, () => {
