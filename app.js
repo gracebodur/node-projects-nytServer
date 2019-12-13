37db599c06efcfd29347abcc03bc8602170765ac
Grace <mgracebodur@gmail.com>
2019-12-13 02:29:35 -0500

    Installed cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(morgan('common')) //common format?
     const books = require('./books-data.js') //require the array of books
 
