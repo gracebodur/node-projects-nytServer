d2e9b1b7a838919bc7e006a95d6a4d54d61f57c4
Grace <mgracebodur@gmail.com>
2019-12-13 01:21:18 -0500

    Initialized basic server format

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('common')) //common format?
+    const books = require('./books-data.js')
+
+app.get('/books', (req, res) => {
+    res
+        .json(books)
+})
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000')
+})
